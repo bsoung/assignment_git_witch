a67a0b71d78f83e9df9aae8444cb6b921a2b9f62
Benjamin Soung <soungbenjamin@gmail.com>
2017-07-31 13:08:23 -0400

    working on command runner

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const gitHubModule = require("./GitData");
+const { run } = require("./lib");
+
+// const gitHubModule = require("./GitData");
+
 function randomfunction() {
-  console.log("am I working");
+	console.log("am I working");
 }
+
+run({
+	username: "griselda",
+	subject: "repos",
+	query: "details"
+});
 randomfunction();
