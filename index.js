3acc26063a57d359bf6d158d1cc6d50291ff3de3
Benjamin Soung <soungbenjamin@gmail.com>
2017-07-31 16:23:47 -0400

    working on modules

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { run } = require("./lib");
+// const { run } = require("./lib");
 
 // const gitHubModule = require("./GitData");
 
@@ -6,9 +6,4 @@ function randomfunction() {
 	console.log("am I working");
 }
 
-run({
-	username: "griselda",
-	subject: "repos",
-	query: "details"
-});
 randomfunction();
